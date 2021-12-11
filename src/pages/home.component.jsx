c1b33a642637e1f6558fa9b8c914ff0872e15fbc
Ankit Sharma <sankit72@gmail.com>
2021-12-12 01:41:02 +0530

    updated the home page

--- src/pages/home.component.jsx
+++ src/pages/home.component.jsx
@@ -1,9 +1,11 @@
 import "./home.styles.css";
 
 const HomePage = () => (
-  <div>
-    <h1>Hi I am Ankit Sharma </h1>
-    <h3>A Full Stack Java Developer</h3>
+  <div className="h-screen flex justify-center items-center  bg-gradient-to-r from-blue-500 to bg-purple-500">
+    <div className="text-white">
+      <p className="text-7xl font-bold tracking-wider">Ankit</p>
+      <p className="text-6xl font-thin tracking-wide">Sharma</p>
+    </div>
   </div>
 );
 
